a722b1d6bacab8ccca69e28eb54cf1c8224077af
Balaguru4580 <balaguru4580@gmail.com>
2020-08-11 10:58:30 +0530

    Animations! Smoothness!

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,17 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { NumberValueAccessor } from '@angular/forms';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
-})
+  styleUrls: ['./board.component.scss'],
+  animations: [
+    trigger('faderino', [
+      state("rest", style({
+        opacity: 1
+      })),
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(250)
+      ]),
+    ]),
+    
+    trigger('drop', [
+      state("rest", style({
+        opacity: 1,
+        transform: 'translateY(-15%)'
+      })),
+      state("final", style({
+        opacity: 0.75,
+        transform: 'translateY(-5%)' 
+      })),
+      transition(':enter', [
+        style({opacity: 0, transform: 'translateY(0)'}),
+        animate('500ms 0s ease-in')
+      ]),
+      transition('rest => final', [
+        animate('200ms')
+      ]),
+      transition('final => rest', [
+        animate('200ms')
+      ])
+    ]),
+  ]})
+
 export class BoardComponent implements OnInit {
   squares: string[];
   xIsNext: boolean;
   winner: string;
   draw: string;
   count: number;
+  currentState = 'rest';
 
   constructor() { }
 
@@ -23,6 +63,7 @@ export class BoardComponent implements OnInit {
     this.squares = Array(9).fill(null);
     this.winner = null;
     this.xIsNext = true;
+    this.currentState = "rest"
   }
 
   get player() {
@@ -36,13 +77,6 @@ export class BoardComponent implements OnInit {
     }
     this.winner = this.calculateWinner();
 
-
-    if (this.winner != null) {
-      var time = 5000;
-      setTimeout(() => { this.newGame(); }, time);
-
-
-    } 3
   }
 
   calculateWinner() {
@@ -68,11 +102,12 @@ export class BoardComponent implements OnInit {
           this.squares[a] === this.squares[c])
       ) {
         x = 5;
-        console.log("BRUH");
+        this.currentState = "final";
         return this.squares[a];
       }
     }
-    if ((this.squares.every(x => x != null))){
+    if ((this.squares.every(x => x != null))) {
+      this.currentState = "final";
       return 'draw';
     }
     return null;
