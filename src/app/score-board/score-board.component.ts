ae406e0064af5f5f96aa7ef933288b2652dd6cd5
Balaguru4580 <balaguru4580@gmail.com>
2020-08-20 14:21:23 +0530

    Title!

--- src/app/score-board/score-board.component.ts
+++ src/app/score-board/score-board.component.ts
@@ -1,21 +1,44 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ScoreTransService } from '../score-trans.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-score-board',
   templateUrl: './score-board.component.html',
-  styleUrls: ['./score-board.component.scss']
+  styleUrls: ['./score-board.component.scss'],
+  animations: [
+    trigger('kickoff', [
+      state("nomove", style({
+        opacity: 1
+      })),
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(1000)
+      ]),
+    ]),
+  ]
 })
 
 export class ScoreBoardComponent implements OnInit {
 
   Score: [number, number];
+  Active: boolean;
 
+  checkActive() {
+    if (this.Score != undefined) {
+      this.Active = true;
+    }
+  }
 
-  constructor(private ScoreData: ScoreTransService) { }
+  constructor(private ScoreData: ScoreTransService) {}
 
   ngOnInit() {
     this.ScoreData.currentScore.subscribe(Xwin => this.Score = Xwin);
   }
-
 }
